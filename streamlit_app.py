e2674977b2c7a3f8b3988644d12048b1b07eea96
albe <albedehombre@icloud.com>
2024-08-07 10:47:32 -0500

    added csv dump and asset

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,28 @@
 import streamlit as st
+import csv
 
-st.title("He has to think about the shop name")
+st.title("Mohji's Shop")
 st.write(
-    "Testing if this updates in real time"
+    "Curtis came up with the name"
 )
+
+def dumpCSV(file='/home/albede/Desktop/Practicum Projects/Streamlit1_Practicum/dev assets/Sanford+Stuff+Catalogue+-+Sheet1.csv'):
+    with open(file, newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        names, descriptions, quantities = [],[],[]
+        
+        for row in spamreader:
+            info = ' '.join(row).split(',')
+            quantities.insert(-1, info[-1])
+            names.insert(-1, info[0])
+
+            des = ''
+            for i in range(1, len(info) - 1): des += info[i]
+            descriptions.insert(-1, des)
+
+        return names, descriptions, quantities
+    return
+
+
+names, descriptions, quantities = dumpCSV()
+print(names[3])
