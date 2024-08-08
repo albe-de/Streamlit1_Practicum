58e02a7e330082a7e5bc8a0807dc68d47e6bda7e
albe <albedehombre@icloud.com>
2024-08-08 10:24:30 -0500

    added data store

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,34 +1,43 @@
+from streamlit_gsheets import GSheetsConnection
 from io import StringIO
 import streamlit as st
 import pandas as pd
 import requests
 import csv
 
+# third party meathods for exess site usage
+class exessMeathods():
+    def __init__(self): 
+        # use github links to access data-->
+        self.csvLocation = 'https://github.com/albe-de/Streamlit1_Practicum/blob/main/dev%20assets/Sanford%2BStuff%2BCatalogue%2B-%2BSheet1.csv'
+
+    def dumpCSV(file=self.csvLocation):
+        response = requests.get(file)
+        response.raise_for_status() 
+        csv_content = response.text
+
+        csvfile = StringIO(csv_content)
+        reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        names, descriptions, quantities = [], [], []
+
+        for row in reader:
+            if not row: continue
+            names.append(row[0])
+            quantities.append(row[-1])
+
+            description = ','.join(row[1:-1])
+            descriptions.append(description)
+        
+        return names, descriptions, quantities
+
 st.title("Mohji's Shop")
 st.write(
     "Curtis came up with the name"
 )
 
-# use github links to access data-->
-csvLocation = 'https://github.com/albe-de/Streamlit1_Practicum/blob/main/dev%20assets/Sanford%2BStuff%2BCatalogue%2B-%2BSheet1.csv'
-def dumpCSV(file=csvLocation):
-    response = requests.get(file)
-    response.raise_for_status() 
-    csv_content = response.text
+# Create a connection object.
+conn = st.connection("gsheets", type=GSheetsConnection)
+df = conn.read()
 
-    csvfile = StringIO(csv_content)
-    reader = csv.reader(csvfile, delimiter=',', quotechar='"')
-    names, descriptions, quantities = [], [], []
-
-    for row in reader:
-        if not row: continue
-        names.append(row[0])
-        quantities.append(row[-1])
-
-        description = ','.join(row[1:-1])
-        descriptions.append(description)
-    
-    return names, descriptions, quantities
-
-names, descriptions, quantities = dumpCSV()
-print(names)
+for row in df.itertuples():
+    st.write(f"{row.name} has a :{row.pet}:")
